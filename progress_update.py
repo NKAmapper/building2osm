32de994d6a34ef998565cfbd99cbffaae0350390
noenandre <5470915+noenandre@users.noreply.github.com>
2021-03-14 23:37:53 +0100

    Added logic for city subdivisions. Fixed bug where old "osm_buildings" was updated before check against new value. Replaced '%s' formatting with f-string formatting. Replaced file.close() with "with" statement

--- progress_update.py
+++ progress_update.py
@@ -20,7 +20,7 @@ version = "0.1.0"
 request_header = {"User-Agent": "osmno/buildings2osm"}
 
 sleep_time = 1000  # Number of buildings per 1 second sleep time before Overpass requests
-
+overpass_instance = "https://overpass.kumi.systems/api/interpreter"  # https://overpass.kumi.systems/api/interpreter
 
 
 # Output message to console
@@ -45,29 +45,29 @@ def try_urlopen (url, header):
 			return urllib.request.urlopen(request)
 		except urllib.error.HTTPError as e:
 			if e.code in [429, 503, 504]:  # Too many requests, Service unavailable or Gateway timed out
-				if tries  == 0:
-					message ("\n") 
-				message ("\rRetry %i in %ss... " % (tries + 1, delay * (2**tries)))
+				if tries == 0:
+					message("\n")
+				message(f"\rRetry {tries + 1:d} in {delay * (2 ** tries)}s... ")
 				time.sleep(delay * (2**tries))
 				tries += 1
 			elif e.code in [401, 403]:
-				message ("\nHTTP error %i: %s\n" % (e.code, e.reason))  # Unauthorized or Blocked
+				message(f"\nHTTP error {e.code:d}: {e.reason}\n")  # Unauthorized or Blocked
 				sys.exit()
 			elif e.code in [400, 409, 412]:
-				message ("\nHTTP error %i: %s\n" % (e.code, e.reason))  # Bad request, Conflict or Failed precondition
-				message ("%s\n" % str(e.read()))
+				message(f"\nHTTP error {e.code:d}: {e.reason}\n")  # Bad request, Conflict or Failed precondition
+				message(f"{str(e.read())}\n")
 				sys.exit()
 			else:
 				raise
 
 		except urllib.error.URLError as e:  # Mostly "Connection timed out"
-			if tries  == 0:
-				message ("\n") 
-			message ("\r\tRetry %i in %ss... " % (tries + 1, delay * (2**tries)))
+			if tries == 0:
+				message("\n")
+			message(f"\r\tRetry {tries + 1:d} in {delay * (2 ** tries)}s... ")
 			time.sleep(delay * (2**tries))
 			tries += 1
-	
-	message ("\nError: %s\n" % e.reason)
+
+	message(f"\nError: {e.reason}\n")
 	sys.exit()
 
 
@@ -76,7 +76,7 @@ def try_urlopen (url, header):
 
 def load_progress_page():
 
-	message ("\nLoading wiki progress page ...\n")
+	message("\nLoading wiki progress page ...\n")
 
 	url = "https://wiki.openstreetmap.org/wiki/Import/Catalogue/Norway_Building_Import/Progress"
 
@@ -86,7 +86,7 @@ def load_progress_page():
 	page.close()
 
 	content = storesoup.find(class_="mw-parser-output")
-	table = content.find("table").find("tbody").find_all("tr")
+	table = content.find("table").find("tbody").find_all("tr", recursive=False)
 
 	for row in table[2:]:
 		cols = row.find_all('td')
@@ -107,18 +107,40 @@ def load_progress_page():
 				'osm_buildings': int(float(cols[4].replace(" ", ""))),
 				'ref_progress': int(float(cols[5].strip("%").replace(" ", ""))),
 				'user': cols[6],
-				'status': cols[7] 
+				'status': cols[7]
 			}
 
-	message ("\t%i municipalities\n" % len(municipalities))
+	message(f"\t{len(municipalities):d} municipalities\n")
 
+	for city_id, has_id in [("0301", True), ("4601", False)]:
+		city_name = municipalities[city_id]["name"]
+		municipalities[city_id]["subdiv"] = []
+		table = content.select_one(f'h2:-soup-contains("{city_name}") + table').find("tbody").find_all("tr", recursive=False)
+
+		for row in table[1:]:
+			cols = row.find_all('td')
+			cols = [ele.text.strip() for ele in cols]
+
+			if not cols[1 + has_id]:
+				cols[1 + has_id] = "0"
+
+			subdiv = {
+				'name': cols[0 + has_id],
+				'osm_buildings': int(cols[1 + has_id].replace(" ", "")),
+				'user': cols[2 + has_id],
+				'status': cols[3 + has_id]
+			}
+			if has_id:
+				subdiv['id'] = cols[0]
+
+			municipalities[city_id]["subdiv"].append(subdiv)
 
 
 # Get buildings from cadastral registry.
 
 def count_import_buildings():
 
-	message ("\nLoading buildings from cadastral registry ...\n")
+	message("\nLoading buildings from cadastral registry ...\n")
 
 	# Namespace
 
@@ -134,12 +156,11 @@ def count_import_buildings():
 
 	for municipality_id, municipality in municipalities.items():
 
-		message ("\t%-20s " % municipality['name'])
+		message(f"\t{municipality['name']:<20} ")
 
 		# Load file from GeoNorge
 
-		url = "https://nedlasting.geonorge.no/geonorge/Basisdata/MatrikkelenBygning/GML/Basisdata_%s_%s_25833_MatrikkelenBygning_GML.zip" \
-				% (municipality_id, municipality['name'])
+		url = f"https://nedlasting.geonorge.no/geonorge/Basisdata/MatrikkelenBygning/GML/Basisdata_{municipality_id}_{municipality['name']}_25833_MatrikkelenBygning_GML.zip"
 		url = url.replace("Æ","E").replace("Ø","O").replace("Å","A").replace("æ","e").replace("ø","o").replace("å","a").replace(" ", "_")
 
 		in_file = urllib.request.urlopen(url)
@@ -162,15 +183,15 @@ def count_import_buildings():
 		for feature in root.iter('{%s}featureMember' % ns_gml):
 			count += 1
 
-		message ("{:,}".format(count).replace(',', ' '))
+		message(f"{count:,}".replace(',', ' '))
 		if count != municipality['import_buildings']:
-			message ("  --> %i" % (count - municipality['import_buildings']))
-		message ("\n")
+			message(f"  ⟶ {count - municipality['import_buildings']:d}")
+		message("\n")
 
 		municipality['import_buildings'] = count
 		total_count += count
 
-	message ("\tTotal %i cadastral buildings in Norway\n" % total_count)
+	message(f"\tTotal {total_count:d} cadastral buildings in Norway\n")
 
 
 
@@ -178,30 +199,27 @@ def count_import_buildings():
 
 def count_osm_buildings():
 
-	message ("\nLoading existing buildings from OSM ...\n")
+	message("\nLoading existing buildings from OSM ...\n")
 
 	total_count = 0
 
 	for municipality_id, municipality in municipalities.items():
 
-		message ("\t%-20s " % municipality['name'])
+		message(f"\t{municipality['name']:<20} ")
 
 		# Get number of buildings
 
-		query = '[out:json][timeout:60];(area[ref=%s][admin_level=7][place=municipality];)->.a;(nwr["building"](area.a););out count;' \
-			 	% municipality_id
+		query = f'[out:json][timeout:60];(area[ref={municipality_id}][admin_level=7][place=municipality];)->.a;(nwr["building"](area.a););out count;'
 
-		url = "https://overpass-api.de/api/interpreter?data=" + urllib.parse.quote(query)
-		file = try_urlopen(url, request_header)
-		data = json.load(file)
-		file.close()
+		url = f'{overpass_instance}?data={urllib.parse.quote(query)}'
+		with try_urlopen(url, request_header) as file:
+			data = json.load(file)
 
 		count = data['elements'][0]['tags']
 		count_buildings = int(count['ways']) + int(count['relations'])
-		municipality['osm_buildings'] = count_buildings
 		total_count += count_buildings
 
-		message ("%-6s " % count_buildings)
+		message(f"{count_buildings:<6} ")
 
 		time.sleep(20 + count_buildings / sleep_time)
 
@@ -210,25 +228,47 @@ def count_osm_buildings():
 		query = '[out:json][timeout:60];(area[ref=%s][admin_level=7][place=municipality];)->.a;(nwr["ref:bygningsnr"](area.a););out count;' \
 			 	% municipality_id
 
-		url = "https://overpass-api.de/api/interpreter?data=" + urllib.parse.quote(query)
-		file = try_urlopen(url, request_header)
-		data = json.load(file)
-		file.close()
+		url = f'{overpass_instance}?data={urllib.parse.quote(query)}'
+		with try_urlopen(url, request_header) as file:
+			data = json.load(file)
 
 		count_tags = int(data['elements'][0]['tags']['total'])
-		message ("%6i %3i%%" % (count_tags, 100.0 * count_tags / municipality['import_buildings']))
+		municipality['ref_progress'] = 100 * count_tags / municipality['import_buildings']
 
-		municipality['ref_progress'] = 100.0 * count_tags / municipality['import_buildings']
+		message(f"{count_tags:6d} {int(municipality['ref_progress']):3d}%")
 
 		# Compare with last update
 
 		if count_buildings != municipality['osm_buildings']:
-			message ("  --> %i" % (count_buildings - municipality['osm_buildings']))
-		message ("\n")
+			message(f"  ⤍ {count_buildings - municipality['osm_buildings']:d}")
+		message("\n")
 
-		time.sleep(20 + count_buildings / sleep_time)
+		municipality['osm_buildings'] = count_buildings
 
-	message ("\tTotal %i OSM buildings in Norway\n" % total_count)
+		if "subdiv" in municipality:
+			for subdiv in municipality["subdiv"]:
+				time.sleep(5 + count_buildings / sleep_time)
+
+				message(f'\t\tBydel {subdiv["name"]:<20}')
+				query = f'[out:json][timeout:60];(area[name="{subdiv["name"]}"][admin_level=9];)->.a;(nwr["building"](area.a););out count;'
+				url = f'{overpass_instance}?data={urllib.parse.quote(query)}'
+				with try_urlopen(url, request_header) as file:
+					data = json.load(file)
+
+				count = data['elements'][0]['tags']
+				count_buildings = int(count['ways']) + int(count['relations'])
+
+				message(f"{count_buildings:>7}")
+
+				if count_buildings != subdiv['osm_buildings']:
+					message(f"  --> {count_buildings - subdiv['osm_buildings']:d}")
+				message("\n")
+
+				subdiv['osm_buildings'] = count_buildings
+
+	time.sleep(20 + count_buildings / sleep_time)
+
+	message("\tTotal %i OSM buildings in Norway\n" % total_count)
 
 
 
@@ -242,31 +282,44 @@ def output_file():
 	import_count = 0
 
 	filename = "import_progress.txt"
-	file = open(filename, "w")
+	with open(filename, "w", encoding='utf-8') as file:
 
-	for municipality_id, municipality in municipalities.items():
+		for municipality_id, municipality in municipalities.items():
 
-		message("\t%s %-15s %-20s %6i %6i %3i%% %-10s %-10s\n" % (municipality_id, municipality['name'], municipality['county'], \
-			municipality['import_buildings'], municipality['osm_buildings'], municipality['ref_progress'], \
-			municipality['user'], municipality['status']))
+			message(f"\t{municipality_id} {municipality['name']:<15} {municipality['county']:<20} {municipality['import_buildings']:6d} {municipality['osm_buildings']:6d} {municipality['ref_progress']:3d}% {municipality['user']:<10} {municipality['status']:<10}\n")
 
-		file.write("|-\n")
-		file.write("|%s\n" % municipality_id)
-		file.write("|%s\n" % municipality['name'])
-		file.write("|%s\n" % municipality['county'])		
-		file.write("|%s\n" % "{:,}".format(municipality['import_buildings']).replace(',', ' '))
-		file.write("|%s\n" % "{:,}".format(municipality['osm_buildings']).replace(',', ' '))
-		file.write("|%i%%\n" % municipality['ref_progress'])
-		file.write("|%s\n" % municipality['user'])
-		file.write("|%s\n" % municipality['status'])	
+			file.write("|-\n")
+			file.write(f"|{municipality_id}\n")
+			file.write(f"|{municipality['name']}\n")
+			file.write(f"|{municipality['county']}\n")
+			file.write(f"|{municipality['import_buildings']:,}\n".replace(',', ' '))
+			file.write(f"|{municipality['osm_buildings']:,}\n".replace(',', ' '))
+			file.write(f"|{municipality['ref_progress']:d}%\n")
+			file.write(f"|{municipality['user']}\n")
+			file.write(f"|{municipality['status']}\n")
 
-		import_count += municipality['import_buildings']
-		osm_count += municipality['osm_buildings']
+			import_count += municipality['import_buildings']
+			osm_count += municipality['osm_buildings']
 
-	message ("\t%-41s %6i %6i\n\n" % ("Total in Norway", import_count, osm_count))
-	message ("\nFile saved to '%s'\n\n" % filename)
+			message(f"\t{'Total in Norway':<41} {import_count:6d} {osm_count:6d}\n\n")
 
-	file.close()
+		message(f"\nFile saved to '{filename}'\n\n")
+
+		for city_id, city in filter(lambda m: "subdiv" in m[1], municipalities.items()):
+			filename = f"import_progress_{city['name']}.txt"
+			with open(filename, "w", encoding='utf-8') as file:
+
+				for subdiv in city["subdiv"]:
+					file.write("|-\n")
+					if 'id' in subdiv:
+						file.write(f"|{subdiv['id']}\n")
+					file.write(f"|{subdiv['name']}\n")
+					file.write(f"|{subdiv['osm_buildings']:,}\n".replace(',', ' '))
+					file.write(f"|{subdiv['user']}\n")
+					file.write(f"|{subdiv['status']}\n")
+
+
+			message(f"\nFile saved to '{filename}'\n\n")
 
 
 
